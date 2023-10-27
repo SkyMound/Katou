d41c3153edeca58f8624c4b3781960a25602d624
KikitoBk <86195847+KikitoBk@users.noreply.github.com>
2023-10-27 13:59:40 +0200

    graph en direct ajouté

--- lectureData/directData.py
+++ lectureData/directData.py
@@ -18,11 +18,65 @@ def recup_port():
         print("No Arduino found")
         return None
     
-Data = recup_port()
-if Data!=None:
-    for i in range(20):
-        print(Data.readline())
-        time.sleep(0.1)
+def animate(i):
+    global X,Y,Z,liste_temps
+    allData = recup_port()
+    data = allData.readline()
+    data = data.strip().split()
+    if len(data)!=0:
+        x = float(data[1].decode())
+        y = float(data[3].decode())
+        z = float(data[5].decode())
+        X.append(x)
+        Y.append(y)
+        Z.append(z)
+        temps_mesure = time.time()
+        liste_temps.append(temps_mesure)
+        temps_reel = temps_mesure - liste_temps[0]
+        print(temps_reel)
+        print(x,y,z)
+        ax1.clear()
+        ax1.plot(liste_temps,X)
+        ax1.plot(liste_temps,Y)
+        ax1.plot(liste_temps,Z)
+        ax1.set_xlabel("Temps (s)")
+        ax1.set_ylabel("Accélération (m/s²)")
+        ax1.set_title("Accélération en fonction du temps")
+        ax1.legend(["X","Y","Z"])
+        ax1.grid()
+
+
+temps_reel = 0
+temps_aquisition = 100
+X = []
+Y = []
+Z = []
+liste_temps = []
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
+    
+
+# if allData!=None:
+#     while temps_reel < temps_aquisition:
+#         data = allData.readline()
+#         data = data.strip().split()
+#         print(data)
+#         if len(data)!=0:
+#             x = float(data[1].decode())
+#             y = float(data[2].decode())
+#             z = float(data[3].decode())
+#             X.append(x)
+#             Y.append(y)
+#             Z.append(z)
+#             temps_mesure = time.time()
+#             liste_temps.append(temps_mesure)
+#             temps_reel = temps_mesure - liste_temps[0]
+#             print(temps_reel)
+#             print(x,y,z)
+
 
 
 
