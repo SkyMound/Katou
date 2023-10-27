71e08afa928b95a289a0d31f61056c6bd6cb781b
SkyMound <crafteurkiller@gmail.com>
2023-10-27 14:01:08 +0200

    Added MMA libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
+#include <Adafruit_MMA8451.h>
 #include "ronron.hpp"
 #include "battement.hpp"
 #include "respiration.hpp"
 #include "miaulement.hpp"
 
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting...");
+  if (! mma.begin()) {
+    Serial.println("Couldnt start");
+  }
+  mma.setRange(MMA8451_RANGE_2_G);
   //miaulementInit();
   mySoftwareSerial.begin(9600, SWSERIAL_8N1, PIN_RX, PIN_TX, false, 256);
   Serial.println(mySoftwareSerial.available());
@@ -22,7 +28,17 @@ void setup() {
 }
 
 void loop() {
+  mma.read();
+  sensors_event_t event; 
+  mma.getEvent(&event);
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print(" ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print(" ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print(" ");
+  Serial.println("m/s^2 ");
   // playSound(RONRON_SOUND_1,30);
-  delay(1000);
+  delay(100);
   // put your main code here, to run repeatedly:
 }
