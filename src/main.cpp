ab42c04c6a1fb4f74822bb6c3be17347e58e6aed
SkyMound <crafteurkiller@gmail.com>
2023-10-27 14:05:07 +0200

    Add string guard

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ void loop() {
 
   /* Display the results (acceleration is measured in m/s^2) */
   
-  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print(" ");
+  Serial.print("acc: "); Serial.print(event.acceleration.x); Serial.print(" ");
   Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print(" ");
   Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print(" ");
   Serial.println("m/s^2 ");
